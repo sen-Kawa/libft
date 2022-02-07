7e181897452f5a1509aa87bcfff760c6ecfa00d4
sen-Kawa <karlaheinzc@gmail.com>
2022-02-07 07:52:46 +0100

    strncmp normi

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,25 +6,20 @@
 /*   By: kaheinz <kaheinz@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/06 17:40:42 by kaheinz           #+#    #+#             */
-/*   Updated: 2022/02/06 17:40:46 by kaheinz          ###   ########.fr       */
+/*   Updated: 2022/02/07 07:52:13 by kaheinz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stdio.h>
 
-int ft_strncmp(const char *s1, const char *s2, size_t n)
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
 
 	i = 0;
 	while (s1[i] != '\0' && s1[i] == s2[i] && i < n)
-		{
-	//		if (i == n)
-	//			return (0);
-		//	s1++;
-		//	s2++;
-			i++;	
-		}
-	return(s1[i] - s2[i]);
+		i++;
+	return (s1[i] - s2[i]);
 }
 
 int	main(void)
@@ -35,5 +30,5 @@ int	main(void)
 	printf("%i\n", ft_strncmp("hez", "hey", 3));
 	printf("%i\n", ft_strncmp("aey", "hey", 3));
 	printf("%i\n", ft_strncmp("hey", "aey", 3));
-	return(0);
+	return (0);
 }
