85d82bdf55d4299c0f6370d89b2c8663a7097f51
Karla Heinz <kaheinz@c2s15d81.42wolfsburg.de>
2022-02-09 21:18:50 +0100

    strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -9,5 +9,26 @@
 /*   Updated: 2022/02/06 17:40:46 by kaheinz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <stdio.h>
 
-int strncmp(const char *str1, const char *str2, size_t n)
+int ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (*s1 != '\0' && *s2 != '\0' && i < n)
+		{
+			if (*s1 == *s2)
+				return (0);
+			s1++;
+			s2++;
+			i++;	
+		}
+	return(1);
+}
+
+int	main(void)
+{
+	printf("%i", ft_strncmp("Hey", "hey", 2));
+	return(0);
+}
