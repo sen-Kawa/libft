a7a4ab7e8e14c01764c5b85d2eeb86470f7b84cc
Karla Heinz <kaheinz@c2s15d81.42wolfsburg.de>
2022-02-12 09:47:12 +0100

    normi memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -1,36 +1,23 @@
-#include <stdio.h>
-//#include "libft.h"
+#include "libft.h"
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	unsigned char *ptr;
-	unsigned char *locatedat;
+	unsigned char	*ptr;
+	unsigned char	*locatedat;
 
 	ptr = (unsigned char *)s;
 	locatedat = NULL;
-
 	while ((s != NULL) && (n--))
 	{
-		if(*ptr != (unsigned char)c)
+		if (*ptr != (unsigned char)c)
 		{
 			ptr++;
 		}
 		else
 		{
 			locatedat = ptr;
-			break;
+			break ;
 		}
 	}
-	return(locatedat);
-}
-
-int	main(void)
-{
-	char s[] = "olas del mar";
-	char *p = ft_memchr(s, 'a', 3);
-
-	if(p == NULL)
-		printf("the character was not found\n");
-	else
-		printf("%s\n", p);
+	return (locatedat);
 }
