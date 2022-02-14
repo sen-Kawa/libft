00aa340ef87002bf1e7b9325fdb4503903a7d869
Karla Heinz <kaheinz@c2s15d81.42wolfsburg.de>
2022-02-14 20:06:08 +0100

    normi memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -10,38 +10,18 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include "libft.h"
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	int	i;
 
 	i = 0;
-	while (n > 0)
+	while (i < n)
 	{
-		if (*(unsigned char*)(s1)) != (*(unsigned char*)(s2))
-			return (s1 - s2);
-		else
-		{
-			i++;
-			n--;
-		}
+		if ((*(unsigned char *)(s1 + i)) != (*(unsigned char *)(s2 + i)))
+			return (*(unsigned char *)(s1 + i) - (*unsigned char *)(s2 + i));
+		i++;
 	}
 	return (0);
-}
-
-int	main(void)
-{
-	char	s[] = "hello";
-	char	s2[] = "hello";
-	int	ret;
-
-	ret = ft_memcmp(s, s2, 4);
-	if (ret < 0)
-		printf("s1 is less than s2");
-	else if (ret > 0)
-		printf("s2 is less than s1");
-	else
-		printf("strings compared are equal");
-	return(0);
 }
