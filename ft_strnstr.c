c4631915da57fe640808a2520ae6f04e519a957d
sen-Kawa <karlaheinzc@gmail.com>
2022-02-19 23:31:31 +0100

    try outs

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: kaheinz <kaheinz@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/16 10:19:52 by kaheinz           #+#    #+#             */
-/*   Updated: 2022/02/16 10:22:07 by kaheinz          ###   ########.fr       */
+/*   Updated: 2022/02/19 23:31:19 by kaheinz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@ char	*ft_strnstr(const char *s1, const char *s2, size_t n)
 	size_t	lengths2;
 
 	lengths2 = ft_strlen(s2);
-	if (*s2)
+	if (*s2 == 0)
 		return ((char *)s1);
 	while (*s1 && n)
 	{
 		if (ft_strncmp(s1, s2, lengths2) == 0)
-			return ((char *)s2);
+			return ((char *)s1);
 		if (n <= lengths2)
 			break ;
 		n--;
