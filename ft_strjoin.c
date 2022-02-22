42baa458f85582a6ec3c348da452079c9224da67
sen-Kawa <karlaheinzc@gmail.com>
2022-02-22 20:54:31 +0100

    first scribbles

---
+++ ft_strjoin.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kaheinz <kaheinz@student.42wolfsburg.de>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/22 19:58:49 by kaheinz           #+#    #+#             */
+/*   Updated: 2022/02/22 20:54:03 by kaheinz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include "libft.h"
+#include <stdio.h>
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	concatenated;
+	size_t	lens1;
+	size_t	lens2;
+	char	concatenated;
+
+	lens1 = ft_strlen(lens1);
+	lens2 = ft_strlen(lens2);
+	concatenated = malloc(lens1 + lens2 + 1);
+	if (!concatenated)
+		return (NULL);
+i	while (!s1)
+		s
+}
+
+int main(void)
+{
+	stringy1[] = "hey";
+	stringy2[] = "there";
+	printf("%s", ft_strjoin(stringy1, stringy2));
+}
