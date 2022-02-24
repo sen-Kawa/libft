ad2622cca5d834125fc6de648337a644b87ac7dc
sen-Kawa <karlaheinzc@gmail.com>
2022-02-24 22:06:15 +0100

    writing the function itself

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: kaheinz <kaheinz@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/24 20:41:15 by kaheinz           #+#    #+#             */
-/*   Updated: 2022/02/24 20:44:31 by kaheinz          ###   ########.fr       */
+/*   Updated: 2022/02/24 22:04:37 by kaheinz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,12 @@
 
 void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
+	unsigned int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		f(i, &s[i]);
+		i++;
+	}
 }
