79359c284ce2473c300aed7dd8002904af7a95dc
sen-Kawa <karlaheinzc@gmail.com>
2022-02-24 20:38:07 +0100

    W.M. tests passed

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,11 +6,25 @@
 /*   By: kaheinz <kaheinz@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/24 15:44:54 by kaheinz           #+#    #+#             */
-/*   Updated: 2022/02/24 15:44:56 by kaheinz          ###   ########.fr       */
+/*   Updated: 2022/02/24 20:37:10 by kaheinz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
+	char	*newstr;
+	int		i;
 
+	i = 0;
+	if (!s || !f)
+		return (NULL);
+	newstr = ft_calloc((ft_strlen(s) + 1), sizeof(char));
+	while (s[i])
+	{
+		newstr[i] = f(i, s[i]);
+		i++;
+	}	
+	return (newstr);
 }
