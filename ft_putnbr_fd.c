f2e6e10eee2b53f35690cf0c4f937c6567f0ac6a
sen-Kawa <karlaheinzc@gmail.com>
2022-02-24 23:23:08 +0100

    function

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: kaheinz <kaheinz@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/24 23:02:26 by kaheinz           #+#    #+#             */
-/*   Updated: 2022/02/24 23:05:14 by kaheinz          ###   ########.fr       */
+/*   Updated: 2022/02/24 23:21:58 by kaheinz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,19 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	write(fd, n, 1);
+	long	len;
+
+	len = n;
+	if (len < 0)
+	{
+		ft_putchar_fd('-', fd);
+		len *= -1;
+	}
+	if (len <= 9)
+		ft_putchar_fd(len + '0', fd);
+	else
+	{
+		ft_putnbr_fd(len / 10, fd);
+		ft_putnbr_fd(len % 10, fd);
+	}
 }
