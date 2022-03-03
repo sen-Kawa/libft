d6d9bfdf7bd3a3fd7208ee7899f35dce37619679
Karla Heinz <kaheinz@c2s15d81.42wolfsburg.de>
2022-03-03 20:49:13 +0100

    added static function for negative

--- ft_itoa.c
+++ ft_itoa.c
@@ -26,6 +26,18 @@ static int	lengnum(int n)
 	return (len);
 }
 
+static char	*negative(long int n, char *str, int len)
+{
+	str[0] = '-';
+	n = -n;
+	while (len-- > 1)
+	{
+		str[len] = n % 10 + '0';
+		n = n / 10;
+	}
+	return (str);
+}
+
 char	*ft_itoa(int n)
 {
 	char	*str;
@@ -35,15 +47,10 @@ char	*ft_itoa(int n)
 	str = ft_calloc(i + 1, sizeof(char));
 	if (!str)
 		return (NULL);
-	if (n == -2147483648)
-		return (ft_strdup("-2147483648"));
 	if (n == 0)
 		str[0] = '0';
-	if (n < 0)
-	{	
-		str[0] = '-';
-		n *= -1;
-	}
+	else if (n < 0)
+		str = negative(n, str, i);
 	while (n > 0)
 	{
 		str[i - 1] = n % 10 + '0';
