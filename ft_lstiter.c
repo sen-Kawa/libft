5cfaed20f40e8213aa4c606d232239b207771054
olin_tp_ar <myemail@archi.com>
2022-06-07 11:55:28 +0200

    lstiter

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -6,8 +6,19 @@
 /*   By: kaheinz <kaheinz@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 00:38:36 by kaheinz           #+#    #+#             */
-/*   Updated: 2022/06/07 00:38:45 by kaheinz          ###   ########.fr       */
+/*   Updated: 2022/06/07 11:52:50 by kaheinz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	if (!lst)
+		return ;
+	while (lst)
+	{
+		f(lst->content);
+		lst = lst->next;
+	}
+}
