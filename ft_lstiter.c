6e9f25b7264c2c7d1f8fdadba075f0a0485f7830
olin_tp_ar <myemail@archi.com>
2022-06-07 22:48:54 +0200

    changes for push_swap

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -6,13 +6,13 @@
 /*   By: kaheinz <kaheinz@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 00:38:36 by kaheinz           #+#    #+#             */
-/*   Updated: 2022/06/07 12:56:37 by kaheinz          ###   ########.fr       */
+/*   Updated: 2022/06/07 15:16:12 by kaheinz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstiter(t_list *lst, int (*f)(void *))
+void	ft_lstiter(t_list *lst, int (f)(int *))
 {
 	if (!lst)
 		return ;
